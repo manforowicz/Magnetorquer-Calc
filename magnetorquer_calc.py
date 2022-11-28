7ce5dc73c6273e8836fce5ccf6111b2bb4697889
Marcin Anforowicz <manforowicz@gmail.com>
2022-11-27 18:12:44 -0800

    Added multiple layer support.

--- magnetorquer_calc.py
+++ magnetorquer_calc.py
@@ -30,7 +30,7 @@ def area_sum_of_spiral(a, b, theta):
     return integrate.quad(lambda t: 0.5*(a - b*t)**2, 0, theta)[0]
 
 
-def spacing_from_length(length, outer_layer):
+def spacing_from_length(length, resistance, outer_layer):
     if outer_layer:
         thickness_in_oz = config.getfloat("OuterLayerThickness")
     else:
@@ -41,17 +41,17 @@ def spacing_from_length(length, outer_layer):
 
     coefficient = config.getfloat("CopperResistivity") / thickness_in_m
 
-    trace_width = length / config.getfloat("Resistance") * coefficient
+    trace_width = length / resistance * coefficient
 
     return trace_width + config.getfloat("GapBetweenTraces")  # Millimeters
 
 
 # Returns max trace length physically possible.
 # Takes outer radius and a function that defines spacing
-def max_trace_length(spacing_from_length):
+def max_trace_length(resistance, outer_layer):
 
     def inner_radius_from_length(length):
-        s = spacing_from_length(length, True)
+        s = spacing_from_length(length, resistance, outer_layer)
         return properties_of_spiral(length, s)[1]
 
     return optimize.brentq(inner_radius_from_length, 0, 1e9)
@@ -88,14 +88,14 @@ def properties_of_spiral(length, spacing):
 
 
 # Finds trace length tha maximizes area-sum
-def find_max_area_sum(outer_layer):
+def find_optimal_spiral(resistance, outer_layer):
 
     # Dummy function to meet requirements of `optimize.minimize_scalar`
     def neg_area_sum_from_length(length):
-        s = spacing_from_length(length, outer_layer)
+        s = spacing_from_length(length, resistance, outer_layer)
         return -properties_of_spiral(length, s)[2]
 
-    max_length = max_trace_length(spacing_from_length)
+    max_length = max_trace_length(resistance, outer_layer)
     # Finds length that gives maximum area-sum
     length = optimize.minimize_scalar(
         neg_area_sum_from_length,
@@ -104,9 +104,9 @@ def find_max_area_sum(outer_layer):
     ).x
 
     # Calculate data from the optimal length
-    spacing = spacing_from_length(length, outer_layer)
+    spacing = spacing_from_length(length, resistance, outer_layer)
     optimal = properties_of_spiral(length, spacing)
-
+    '''
     KiCad_spiral.save_curve_kicad(config.getfloat(
         'OuterRadius'), spacing, optimal[0], (spacing-config.getfloat("GapBetweenTraces"))*10)
     print("Saved optimzed spiral SVG to current directory.\n")
@@ -126,17 +126,47 @@ def find_max_area_sum(outer_layer):
         "Number of coils (#):                         {:.4f}\n".format(optimal[0]))
     print(
         "Area sum of coils (m^2):                    {:.4f}\n".format(optimal[2] * 1e-6))
+    '''
+    return length, spacing, optimal[2]
 
 
+def inner_resistance_from_outer(outer_resistance):
+    return (
+            (config.getfloat("Resistance") - 2 * outer_resistance) /
+            (config.getfloat("NumberOfLayers") - 2)
+            )
+
+
+def find_total_area_sum_from_outer_resistance(outer_resistance):
+    inner_resistance = inner_resistance_from_outer(outer_resistance)
+    inner_layers = config.getfloat("NumberOfLayers") -2
+
+    area_sum = 0
+    area_sum += 2 * find_optimal_spiral(outer_resistance, True)[2]
+    area_sum += inner_layers * find_optimal_spiral(inner_resistance, False)[2]
+    return area_sum
+
+
+def find_optimal_magnetorquer():
+    outer_resistance = optimize.minimize_scalar(
+        lambda r: -find_total_area_sum_from_outer_resistance(r),
+        bounds=(0, config.getfloat("Resistance")/2),
+        method='bounded'
+    ).x
+    return outer_resistance
+    
+
+
+'''
 # Draws a nice little graph
 def draw_graph():
     length_array = []
     area_sum_array = []
 
-    max_length = max_trace_length(spacing_from_length)
+    max_length = max_trace_length(config.getfloat('resistance'), True)
     for length in np.linspace(0, max_length, 50):
 
-        spacing = spacing_from_length(length, True)
+        spacing = spacing_from_length(length, config.getfloat('resistance'), True)
         area_sum = properties_of_spiral(length, spacing)[2]
 
         length_array.append(length)
@@ -148,7 +178,7 @@ def draw_graph():
     plt.show()
 
 
-'''
+
 def test_func_properties_of_spiral():
     # Circle with 2 coils.
     result = properties_of_spiral(1, 4 * math.pi, 0)
@@ -172,6 +202,10 @@ config = config['Configuration']
 
 ### BEGIN ###
 if __name__ == "__main__":
+    print("Calculating...")
     # test_func_properties_of_spiral()
-    find_max_area_sum(True)
-    draw_graph()
+    #find_optimal_spiral(config.getfloat('Resistance'), True)
+
+    print(find_optimal_magnetorquer())
+
+
