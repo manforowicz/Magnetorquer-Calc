30c4dba74d8361f0987676caa3e85b47f29621bd
Marcin Anforowicz <manforowicz@gmail.com>
2022-12-03 08:55:08 -0800

    Added autoscaling to square vs circle graph.

--- Square_vs_Circle.py
+++ Square_vs_Circle.py
@@ -1,7 +1,7 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider, Button
+from matplotlib.widgets import Slider
 from scipy import integrate
 from scipy import optimize
 
@@ -14,6 +14,7 @@ def length_of_circle_spiral(a, b, theta):
 # Returns sum of coil areas
 def area_sum_of_circle_spiral(a, b, theta):
     return integrate.quad(lambda t: 0.5*(a - b*t)**2, 0, theta)[0]
+
 
 def properties_of_circle_spiral(length, spacing, outer_radius):
 
@@ -33,16 +34,17 @@ def properties_of_circle_spiral(length, spacing, outer_radius):
 
     # Calculate and return other properties from theta
     inner_radius = a - b*theta
-    if inner_radius < 0: return float('nan')
+    if inner_radius < 0:
+        return float('nan')
     area_sum = area_sum_of_circle_spiral(a, b, theta)
 
-    return  area_sum
+    return area_sum
 
 
 def properties_of_square_spiral(length, spacing, outer_radius):
     r = outer_radius
     s = spacing
-    
+
     length -= r*2
     area_sum = r**2
 
@@ -52,38 +54,56 @@ def properties_of_square_spiral(length, spacing, outer_radius):
             area_sum += 2 * r**2
 
             r -= s/2
-            if r < 0: return float('nan')
+            if r < 0:
+                return float('nan')
 
         r += s/2
     if length < -2*r:
         area_sum -= r ** 2
         length += 2*r
-    
+
     area_sum -= -length * r / 2
-    
+
     return area_sum
 
 
-# Define initial parameters
-init_spacing = 0.5
-init_length = 800
-outer_radius = 10
+#### PLOTTING THE GRAPH ####
 
-lengths = np.linspace(0, init_length, 50)
-areas_square = [properties_of_square_spiral(length, init_spacing, outer_radius) for length in lengths]
-areas_circle = [properties_of_circle_spiral(length, init_spacing, outer_radius) for length in lengths]
+def get_data(spacing):
+    if spacing == 0:
+        max_l = 50000
+    else:
+        max_l = 450/spacing
+
+    lengths = np.linspace(0, max_l, 50)
+    areas_square = [
+        properties_of_square_spiral(l, spacing, OUTER_RADIUS)
+        for l in lengths
+    ]
+    areas_circle = [
+        properties_of_circle_spiral(l, spacing, OUTER_RADIUS)
+        for l in lengths
+    ]
+    return max_l, lengths, areas_square, areas_circle
+
+
+# Define initial parameters
+INIT_SPACING = 0.5
+OUTER_RADIUS = 10
+
 
 # Create the figure and the line that we will manipulate
 fig, ax = plt.subplots()
-ax.set_title("Note: radius is set to 10 units")
-line_square = ax.scatter(lengths, areas_square, label='Square spiral')
-line_circle = ax.scatter(lengths, areas_circle, label="Circular spiral")
-ax.legend()
 
+_, lengths, areas_square, areas_circle = get_data(INIT_SPACING)
+line_square,  = ax.plot(lengths, areas_square, '-o', label='Square spiral')
+line_circle, = ax.plot(lengths, areas_circle, '-o', label="Circular spiral")
 
-
+ax.set_title("radius is set to 10 units")
 ax.set_xlabel('Length')
 ax.set_ylabel('Area-sum')
+ax.legend()
+
 
 # adjust the main plot to make room for the sliders
 fig.subplots_adjust(left=0.25, bottom=0.25)
@@ -95,34 +115,21 @@ spacing_slider = Slider(
     label='Spacing',
     valmin=0,
     valmax=1,
-    valinit=init_spacing,
-)
-
-
-ax_length = fig.add_axes([0.25, 0.05, 0.6, 0.04])
-length_slider = Slider(
-    ax=ax_length,
-    label='Max length (zoom)',
-    valmin=1,
-    valmax=10000,
-    valinit=init_length,
+    valinit=INIT_SPACING,
 )
 
 
 # The function to be called anytime a slider's value changes
 def update(_):
-    s = spacing_slider.val
-    max_l = length_slider.val
-    lengths = np.linspace(0, max_l, 50)
-    areas_square = [properties_of_square_spiral(length, s, outer_radius) for length in lengths]
-    areas_circle = [properties_of_circle_spiral(length, s, outer_radius) for length in lengths]
 
-    line_square.set_offsets(np.c_[lengths, areas_square])
-    line_circle.set_offsets(np.c_[lengths, areas_circle])
+    max_l, lengths, areas_square, areas_circle = get_data(spacing_slider.val)
+
+    line_square.set_data(lengths, areas_square)
+    line_circle.set_data(lengths, areas_circle)
 
     max_y = max(areas_square)
 
-    ax.set_xlim(-0.05*max_l, 1.05*max_l)
+    ax.set_xlim(-0.05*max_l, max_l)
     ax.set_ylim(-0.05*max_y, 1.05*max_y)
 
     fig.canvas.draw_idle()
@@ -130,7 +137,5 @@ def update(_):
 
 # register the update function with each slider
 spacing_slider.on_changed(update)
-length_slider.on_changed(update)
-
 
 plt.show()
